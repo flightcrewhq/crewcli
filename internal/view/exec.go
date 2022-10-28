d77f469699232314817ce8e6e544a054bfcbc9f9
Chris Lee <chris@flightcrew.io>
2022-10-27 21:42:06 -0700

    Use MultiWriter instead of my own wrapped writer

--- internal/view/exec.go
+++ internal/view/exec.go
@@ -26,50 +26,22 @@ func NewExecCommand(s *CommandState) *ExecCommand {
 func (c *ExecCommand) Run() error {
 	debug.Output("run command: %s", c.state.Command)
 	err := c.cmd.Run()
-	stdout := c.stdout.String()
-	stderr := c.stderr.String()
-	c.state.Stdout = stdout
-	c.state.Stderr = stderr
+	c.state.Stdout = c.stdout.String()
+	c.state.Stderr = c.stderr.String()
 	if err != nil {
 		c.state.ErrorMessage = err.Error()
 	}
-	debug.Output("err: %v\nstdout (%d): %s\nstderr (%d): %s\n", err, c.stdout.Len(), stdout, c.stderr.Len(), stderr)
+	debug.Output("err: %v\nstdout (%d): %s\nstderr (%d): %s\n", err, len(c.state.Stdout), c.state.Stdout, len(c.state.Stderr), c.state.Stderr)
 	return err
 }
 func (c *ExecCommand) SetStdin(r io.Reader) {
 	c.cmd.Stdin = r
 }
 func (c *ExecCommand) SetStdout(w io.Writer) {
-	debug.Output("set stdout")
-	c.cmd.Stdout = &wrapOutput{
-		real: w,
-		buf:  &c.stdout,
-	}
-	debug.Output("stdout is %p", c.cmd.Stdout)
+	c.cmd.Stdout = io.MultiWriter(w, &c.stdout)
 }
 func (c *ExecCommand) SetStderr(w io.Writer) {
-	debug.Output("set stderr")
-	c.cmd.Stderr = &wrapOutput{
-		real: w,
-		buf:  &c.stderr,
-	}
-	debug.Output("stderr is %p", c.cmd.Stderr)
-}
-
-// Captures output to a buffer but also writes to the real io writer.
-type wrapOutput struct {
-	buf  *bytes.Buffer
-	real io.Writer
-}
-
-func (wo *wrapOutput) Write(p []byte) (int, error) {
-	debug.Output("Write %s for %p", string(p), wo)
-	n, err := wo.buf.Write(p)
-	debug.Output("Write to buf (%d, %v)", n, err)
-	r, err := wo.real.Write(p)
-	debug.Output("Write to real (%d, %v)", r, err)
-
-	return n, err
+	c.cmd.Stderr = io.MultiWriter(w, &c.stderr)
 }
 
 var (
