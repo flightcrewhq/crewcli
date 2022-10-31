41ea17a17d68c9244b165aca815d853358451adf
Chris Lee <chris@flightcrew.io>
2022-10-31 12:08:59 -0700

    Add tests for getting project, move files

---
+++ internal/gcp/project_test.go
@@ -0,0 +1,54 @@
+package gcp
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseProjectsCSV(mainT *testing.T) {
+	mainT.Run("parse valid csv should succeed", func(t *testing.T) {
+		buf := bytes.NewBufferString(`project_id
+project-1
+some-project
+yeah
+123-project
+`)
+		projects, err := parseListProjectsCSV(buf)
+		assert.NoError(t, err)
+		assert.ElementsMatch(t, []string{
+			"project-1",
+			"some-project",
+			"yeah",
+			"123-project",
+		}, projects)
+	})
+
+	mainT.Run("parse valid empty csv should succeed", func(t *testing.T) {
+		buf := bytes.NewBufferString(`project_id
+`)
+		projects, err := parseListProjectsCSV(buf)
+		assert.NoError(t, err)
+		assert.Empty(t, projects)
+	})
+
+	mainT.Run("parse variable entries should succeed", func(t *testing.T) {
+		buf := bytes.NewBufferString(`something,project_id
+,project-1
+123,project-2
+	`)
+		projects, err := parseListProjectsCSV(buf)
+		assert.NoError(t, err)
+		assert.ElementsMatch(t, []string{
+			"project-1",
+			"project-2",
+		}, projects)
+	})
+
+	mainT.Run("parse invalid csv should error", func(t *testing.T) {
+		buf := bytes.NewBufferString(`ERROR: imagine something went wrong`)
+		_, err := parseListProjectsCSV(buf)
+		assert.Error(t, err)
+	})
+}
